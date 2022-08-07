fcdf327c62f03ad4e38cb4b403ba6abbf048e1f2
Nikolas2610 <npsillou@gmail.com>
2022-08-07 17:45:02 +0300

    Live Heroku

--- src/common/helper/config.schema.ts
+++ src/common/helper/config.schema.ts
@@ -1,6 +1,8 @@
 import * as Joi from '@hapi/joi'
 
 export const configValidationSchema = Joi.object({
+    STAGE: Joi.string().required(),
+    PORT: Joi.number().default(3000),
     DB_HOST: Joi.string().required(),
     DB_PORT: Joi.number().default(5432).required(),
     DB_USERNAME: Joi.string().required(),
